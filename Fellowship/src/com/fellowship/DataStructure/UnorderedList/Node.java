20d8c6fbc9966048a1f77fc1046b2991824a7c27
ansar010 <ansaruddeen030@gmail.com>
2018-12-31 19:19:17 +0530

    commit new

---
+++ Fellowship/src/com/fellowship/DataStructure/UnorderedList/Node.java
@@ -0,0 +1,13 @@
+package com.fellowship.DataStructure.UnorderedList;
+
+public class Node<T>
+{
+	private int int_data;
+	private String str_data;
+	private Node next;
+
+	public Node(int data)
+	{
+		
+	}
+}
